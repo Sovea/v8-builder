a18f3c259374ed293ba513fe7826086595add6ba
Sovea\SoveaZhang <harleyhhh423@gmail.com>
2023-10-10 01:11:37 +0800

    fix: change project build path

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ SHORT_ARCH=$6
 BUILD_TYPE=$7
 
 # Conf
-if [[ "$SHORT_PLATFORM" == "linux" || "$SHORT_PLATFORM" == "android" ]]; then
+if [[ "$SHORT_PLATFORM" = "linux" || "$SHORT_PLATFORM" = "android" ]]; then
     sudo apt-get update -y 1> nul
     sudo apt-get upgrade -y 1> nul
     sudo apt-get update -y 1> nul
@@ -22,18 +22,10 @@ if [[ "$SHORT_PLATFORM" == "linux" || "$SHORT_PLATFORM" == "android" ]]; then
     sudo apt-get upgrade -y 1> nul
     sudo apt-get update -y 1> nul
     sudo apt-get upgrade -y 1> nul
-else if [ "$SHORT_PLATFORM" = "mac" ]; then
-    exit 1
-    brew update
-    brew upgrade
-    brew update
-    brew upgrade
-    brew install pkg-config git curl wget python3 xz zip
-    brew update
-    brew upgrade
-    brew update
-    brew upgrade
-fi
+elif [ "$SHORT_PLATFORM" = "mac" ]; then
+#    brew update
+#    brew upgrade
+    brew install pkg-config git curl wget python@3.8 xz zip ninja
 fi
 
 git config --global user.name "V8 Builder" 1> nul
@@ -41,9 +33,9 @@ git config --global user.email "v8.builder@localhost" 1> nul
 git config --global core.autocrlf false 1> nul
 git config --global core.filemode false 1> nul
 
-cd ~
+cd $REPO
 echo "=====[ Getting Depot Tools ]====="
-if [ $SHORT_PLATFORM = "win" ]; then    
+if [ "$SHORT_PLATFORM" = "win" ]; then
     powershell -command "Invoke-WebRequest https://storage.googleapis.com/chrome-infra/depot_tools.zip -O depot_tools.zip" 1> nul
     7z x depot_tools.zip -o* 1> nul
     export DEPOT_TOOLS_WIN_TOOLCHAIN=0
@@ -52,18 +44,22 @@ else
 fi
 export PATH=$(pwd)/depot_tools:$PATH
 gclient
+cd depot_tools
 update_depot_tools
 # Build
 
+cd $REPO
 mkdir v8
 cd v8
 
 echo "=====[ Fetching V8 ]====="
+pwd
 fetch v8
 echo "target_os = ['$SHORT_PLATFORM']" >> .gclient
-cd ~/v8/v8
+cd v8
+pwd
 git checkout $VERSION
-if [[ "$SHORT_PLATFORM" == "linux" || "$SHORT_PLATFORM" == "android" ]]; then
+if [[ "$SHORT_PLATFORM" = "linux" || "$SHORT_PLATFORM" = "android" ]]; then
     ./build/install-build-deps.sh --no-syms --no-nacl --no-prompt
 fi
 gclient sync
@@ -75,12 +71,12 @@ fi
 echo "=====[ Building V8 ]====="
 
 ARGS=""
-if [ $SHORT_PLATFORM = "win" ]; then
+if [ "$SHORT_PLATFORM" = "win" ]; then
 ARGS="is_clang=true use_lld=false"
 sed -i 's/"-Wmissing-field-initializers",/"-Wmissing-field-initializers","-D_SILENCE_ALL_CXX20_DEPRECATION_WARNINGS",/' BUILD.gn
 fi
 
-if [ $BUILD_TYPE = "monolith" ]; then
+if [ "$BUILD_TYPE" = "monolith" ]; then
 python ./tools/dev/v8gen.py $ARCH -vv --no-goma -- $ARGS target_os=\"$SHORT_PLATFORM\" target_cpu=\"$SHORT_ARCH\" v8_target_cpu=\"$SHORT_ARCH\" is_component_build=false use_goma=false v8_monolithic=true use_custom_libcxx=false v8_enable_sandbox=false v8_enable_i18n_support=true v8_use_external_startup_data=false
 else
 python ./tools/dev/v8gen.py $ARCH -vv --no-goma -- $ARGS target_os=\"$SHORT_PLATFORM\" target_cpu=\"$SHORT_ARCH\" v8_target_cpu=\"$SHORT_ARCH\" is_component_build=false use_goma=false v8_monolithic=false use_custom_libcxx=false v8_enable_sandbox=false v8_enable_i18n_support=true v8_use_external_startup_data=false
@@ -92,35 +88,35 @@ ninja -C out.gn/$ARCH
 # ZIP
 
 cd $REPO
-mkdir -p ~/v8_zip 1> nul
-cp -r ~/v8/v8/include ~/v8_zip 1> nul
+mkdir -p $REPO/v8_zip 1> nul
+cp -r $REPO/v8/v8/include $REPO/v8_zip 1> nul
 
 # Disable "exit on error"
 set +e
-find ~/v8/v8/out.gn/$ARCH -type f -maxdepth 1
-find ~/v8/v8/out.gn/$ARCH/obj -type f -maxdepth 1
-cp ~/v8/v8/out.gn/$ARCH/args.gn ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/icudtl.dat ~/v8_zip
+find $REPO/v8/v8/out.gn/$ARCH -type f -maxdepth 1
+find $REPO/v8/v8/out.gn/$ARCH/obj -type f -maxdepth 1
+cp $REPO/v8/v8/out.gn/$ARCH/args.gn $REPO/v8_zip
+cp $REPO/v8/v8/out.gn/$ARCH/icudtl.dat $REPO/v8_zip
 
-cp ~/v8/v8/out.gn/$ARCH/*.exe ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/*.pdb ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/*.dll ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/*.lib ~/v8_zip
+cp $REPO/v8/v8/out.gn/$ARCH/*.exe $REPO/v8_zip
+cp $REPO/v8/v8/out.gn/$ARCH/*.pdb $REPO/v8_zip
+cp $REPO/v8/v8/out.gn/$ARCH/*.dll $REPO/v8_zip
+cp $REPO/v8/v8/out.gn/$ARCH/*.lib $REPO/v8_zip
 
 if [ $BUILD_TYPE = "monolith" ]; then
-cp ~/v8/v8/out.gn/$ARCH/obj/libv8_monolith.a ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/obj/v8_monolith.lib ~/v8_zip
+cp $REPO/v8/v8/out.gn/$ARCH/obj/libv8_monolith.a $REPO/v8_zip
+cp $REPO/v8/v8/out.gn/$ARCH/obj/v8_monolith.lib $REPO/v8_zip
 else
-cp ~/v8/v8/out.gn/$ARCH/obj/libv8*.a ~/v8_zip
-cp ~/v8/v8/out.gn/$ARCH/obj/v8*.lib ~/v8_zip
+cp $REPO/v8/v8/out.gn/$ARCH/obj/libv8*.a $REPO/v8_zip
+cp $REPO/v8/v8/out.gn/$ARCH/obj/v8*.lib $REPO/v8_zip
 fi
-cp ~/v8/v8/out.gn/$ARCH/obj/*.dylib ~/v8_zip
+cp $REPO/v8/v8/out.gn/$ARCH/obj/*.dylib $REPO/v8_zip
 # Enable "exit on error"
 set -e
 
 
 if [ $SHORT_PLATFORM != "win" ]; then
-zip -r v8_$PLATFORM.zip ~/v8_zip/* 1> nul
+zip -r v8_$PLATFORM.zip $REPO/v8_zip/* 1> nul
 else
-7z a v8_$PLATFORM.zip ~/v8_zip/* 1> nul
+7z a v8_$PLATFORM.zip $REPO/v8_zip/* 1> nul
 fi
